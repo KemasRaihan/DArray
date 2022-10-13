977e46a18a58ab8a0227996928213b40f8fe8f8b
Raihan Madani <74256544+KemasRaihan@users.noreply.github.com>
2022-10-13 08:14:58 +0100

    Initial commit

---
+++ DArray.cpp
@@ -0,0 +1,178 @@
+#include "DArray.h"
+#include <iostream>
+#include <crtdbg.h>
+
+using namespace std;
+
+DArray::DArray()
+{
+	numberOfItems = 0;
+	maxcap = 10;
+	data = (float*)malloc(sizeof(float) * maxcap);
+}
+
+
+DArray::~DArray()
+{
+	free(data);
+}
+
+void DArray::addItem(float item)
+{
+	/*grow array*/
+	if (numberOfItems == maxcap)
+	{
+		maxcap *= 4;
+		data = (float*)realloc(data, sizeof(float) * maxcap);
+	}
+
+	data[numberOfItems++] = item; // add item to the array 
+}
+
+void DArray::removeItem(int index)
+{
+	try{
+		if (index < 0 || index >= numberOfItems)
+		{
+			throw index;
+		}
+		else 
+		{
+			for (int i = index; i < numberOfItems; i++)
+			{
+				data[i] = data[i + 1];
+			}
+			numberOfItems--;
+		}
+	}
+	catch (int index)
+	{
+		cout << "Index " << index << " is out of bounds";
+	}
+}
+
+void DArray::display()
+{
+	cout << "\n[";
+	for (int i = 0; i < numberOfItems; i++)
+	{
+		cout << data[i] << ",";
+	}
+	cout << "]\n";
+}
+
+
+DArray* DArray::add(DArray* rhs)
+{
+	DArray* result = new DArray();
+	if (numberOfItems == rhs->numberOfItems)
+	{
+		for (int i = 0; i < numberOfItems; i++)
+		{
+			//result->data[i] = data[i] + rhs->data[i];
+			// the code below works because numberOfItems parameter is incremented when add() is used
+			result->addItem(data[i] + rhs->data[i]); 
+		}
+		return result;
+	}
+
+	return nullptr;
+
+}
+
+float DArray::calculateMagnitude()
+{
+	float sumOfSquares = 0;
+
+	for (int i = 0; i < numberOfItems; i++)
+	{
+		sumOfSquares += data[i] * data[i];
+	}
+
+	float magnitude = sqrt(sumOfSquares);
+	//cout << "\nMagnitude is " << magnitude;
+
+	return magnitude;
+}
+
+DArray* DArray::normalise()
+{
+	DArray* result = new DArray();
+	for (int i = 0; i < numberOfItems; i++)
+	{
+		result->data[i] = data[i] / calculateMagnitude();
+	}
+
+	return result;
+}
+
+float DArray::dotProduct(DArray* rhs)
+{
+	float result = 0;
+
+	if (numberOfItems == rhs->numberOfItems)
+	{
+		for (int i = 0; i < numberOfItems; i++)
+		{
+			result += data[i] * rhs->data[i];
+		}
+		return result;
+	}
+
+	return -1;
+}
+
+int main()
+{
+#ifdef _DEBUG
+	//  _CrtSetBreakAlloc(87);
+	_onexit(_CrtDumpMemoryLeaks);
+#endif
+
+	DArray* d1 = new DArray();
+	d1->addItem(2.0f);
+	d1->addItem(8.0f);
+	d1->addItem(7.0f);
+	d1->addItem(2.5f);
+
+	cout << "\nd1 before removal : ";
+	d1->display();
+
+	d1->removeItem(4);
+	cout << "\nd1 after removal : ";
+	d1->display();
+
+	//DArray* d2 = new DArray();
+	//d2->addItem(-1.0f);
+	//d2->addItem(15.0f);
+	//d2->addItem(-7.0f);
+	//d2->addItem(0.0f);
+
+	//cout << "\nd2 : ";
+	//d2->display();
+
+	//DArray* d3 = d1->add(d2);
+
+	//cout << "\nd3 = d1 + d2 : ";
+	//d3->display();
+
+	float md1 = d1->calculateMagnitude();
+	cout << "\nThe magnitude of d1 is " << md1 << "\n";
+
+	//float dd12 = d1->dotProduct(d2);
+	//cout << "\nThe dot product of d1 and d2 is " << dd12 << "\n";
+
+
+
+	
+	delete d1;
+	d1 = nullptr;
+
+	//delete d2;
+	//d2 = nullptr;
+
+	//delete d3;
+	//d3 = nullptr;
+
+	return 0;
+}
---
+++ DArray.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class DArray
+{
+private:
+	int numberOfItems, maxcap;
+	float* data;
+	float magnitude;
+
+public:
+	DArray();
+	~DArray();
+	void addItem(float item);
+	void removeItem(int index);
+	void display();
+	DArray* add(DArray* rhs);
+	float calculateMagnitude();
+	DArray* normalise();
+	float dotProduct(DArray* rhs);
+};
+
